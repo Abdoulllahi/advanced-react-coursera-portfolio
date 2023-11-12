af4a40350b65508ee40a64a441e0797f584bee00
Abdoulllahi <dabdoulahi@gmail.com>
2023-11-11 23:51:33 -0600

    removing comments

--- src/components/LandingSection.jsx
+++ src/components/LandingSection.jsx
@@ -2,12 +2,6 @@ import React from "react";
 import { Avatar, Heading, VStack } from "@chakra-ui/react";
 import FullScreenSection from "./FullScreenSection";
 
-const greeting = "Hello, I am Abdu!";
-const bio1 = "A frontend developer";
-const bio2 = "specialised in React";
-
-// Implement the UI for the LandingSection component according to the instructions.
-// Use a combination of Avatar, Heading and VStack components.
 const LandingSection = () => (
   <FullScreenSection
     justifyContent="center"
